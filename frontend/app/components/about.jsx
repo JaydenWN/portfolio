df356bd92e74f327c26e1767f00adb488bab06fc
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-12-06 17:43:18 +1100

    fixed navbar appearing behind css container

--- frontend/app/components/about.jsx
+++ frontend/app/components/about.jsx
@@ -1,19 +1,23 @@
 import styles from './styles/about.module.css'
-import { FaChevronDown } from "react-icons/fa/index.js";
+import { FaChevronDown, FaGithub} from "react-icons/fa/index.js";
+import { Link } from '@remix-run/react';
 
 export default function About(){
 
     return(
-        <>
+        <div id={styles['about__container']}>
         <section id={styles['about__section']}>
             <div></div>
             <p>Hello,</p>
             <p>I'm Jayden Naylon.</p>
             <p>I'm a Junior Web Developer.</p>
+            <span>
             <button>View my projects <FaChevronDown /></button>
+            <Link to={'https://github.com/JaydenWN'}><FaGithub/></Link>
+            </span>
         </section>
 
-        </>
+        </div>
     )
 
 }
