916572f05cea369fd4db90e330a4c0b5e7e85eec
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-12-06 18:52:49 +1100

    responsive and style changes to about landing hero

--- frontend/app/components/about.jsx
+++ frontend/app/components/about.jsx
@@ -5,19 +5,22 @@ import { Link } from '@remix-run/react';
 export default function About(){
 
     return(
-        <div id={styles['about__container']}>
+        
+            
         <section id={styles['about__section']}>
-            <div></div>
-            <p>Hello,</p>
-            <p>I'm Jayden Naylon.</p>
-            <p>I'm a Junior Web Developer.</p>
-            <span>
-            <button>View my projects <FaChevronDown /></button>
-            <Link to={'https://github.com/JaydenWN'}><FaGithub/></Link>
-            </span>
+        <div id={styles['about__image']}></div>
+            <div id={styles['about__content']}>
+                <p>Hello,</p>
+                <p>I'm Jayden Naylon.</p>
+                <p>I'm a Junior Web Developer.</p>
+                <span>
+                <button>View my projects <FaChevronDown /></button>
+                <Link to={'https://github.com/JaydenWN'}><FaGithub/></Link>
+                </span>
+            </div>
         </section>
 
-        </div>
+       
     )
 
 }
