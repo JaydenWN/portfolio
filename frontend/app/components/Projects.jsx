22cf2ff859def49bb03f4dac5465cdec645f2261
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-12-07 14:34:56 +1100

    created project_card component

---
+++ frontend/app/components/Projects.jsx
@@ -0,0 +1,22 @@
+import Project_card from "./Project_card";
+import styles from './styles/projects.module.css'
+
+export default function Projects(){
+
+    return(
+        <section id={styles['project__section']}>
+            <h1>Recent Projects</h1>
+            <div className="project__list">
+                <Project_card 
+                    title={'Project 1'}
+                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error excepturi dolorum dolores amet cupiditate omnis cum earum soluta optio debitis suscipit unde doloremque aut, ipsum numquam architecto beatae iste et. Fugit impedit'}/>
+                <Project_card 
+                    title={'Project 2'}
+                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error excepturi dolorum dolores amet cupiditate omnis cum earum soluta optio debitis suscipit unde doloremque aut, ipsum numquam architecto beatae iste et. Fugit impedit'}/>
+                <Project_card 
+                    title={'Project 3'}
+                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error excepturi dolorum dolores amet cupiditate omnis cum earum soluta optio debitis suscipit unde doloremque aut, ipsum numquam architecto beatae iste et. Fugit impedit'}/>
+            </div>
+        </section>
+    )
+}
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import styles from '../components/styles/header.module.css'
 import About from '../components/about.jsx'
+import Projects from '../components/Projects.jsx'
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,7 +13,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <About/>
+      
+      <Projects/>
     </>
   );
 }
